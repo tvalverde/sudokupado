328e073f780fcfa97eef0d2ec065e71cb5b9c4a8
Toni Valverde <tvalverde@gmail.com>
2026-05-15 02:18:49 +0200

    refactor: optimize react build pipeline by switching to official vite-react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'node:path';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
